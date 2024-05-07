ff020f9d3e6b543a3f8233bd0341b53c30130658
Harley Ewert <harleyewert@Harleys-MacBook-Pro.local>
2024-05-07 16:33:03 -0600

    removed numpy and pandas from reqs

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 import datetime 
 from flask import request
 import math
